19383ef43876168553099a8c1eed106dcfa3accd
ilovemeyou2000@163.com <ilovemeyou2000@163.com>
2016-12-07 01:43:21 +0800

    InitTaoke

---
+++ WebCenter.Web/Code/TaoKeHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Top.Api;
+
+namespace WebCenter.Web.Code
+{
+    public class TaoKeHelper
+    {
+        public TaoKeHelper()
+        {
+            var  serverUrl = "";
+            var appKey = "";
+            var secret = "";
+            this.client = new DefaultTopClient(serverUrl,appKey,secret);
+        }
+
+
+        private ITopClient client
+        {
+            get;
+            set;
+        }
+
+        public JsonNetResult getTbkUatmFavoritesItem()
+        {
+        }
+    }
+}
